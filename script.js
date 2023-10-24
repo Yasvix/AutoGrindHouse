171264b4433b6ba416eb83f2af4cc3ece89b2ac3
Yasvix <j.lyszkowski@o2.pl>
2023-10-24 21:11:11 +0200

    Slider section mobile done

--- script.js
+++ script.js
@@ -17,4 +17,60 @@ btnOpen.addEventListener('click', function() {
 
 
 
+///////////////////////////////////////
+// Slider
 
+const slider = function() {
+    const slides = document.querySelectorAll('.slide');
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+
+    let curSlide = 0;
+    const maxSlide = slides.length;
+
+    const disableXScroll = function() {
+        document.body.style.overflowX = 'hidden';
+
+    }
+
+    const goToSlide = function(slide) {
+        slides.forEach(
+            (s, i) => (s.style.transform = `translateX(${125 * (i - slide)}%)`)
+        );
+    };
+
+    const nextSlide = function() {
+        if(curSlide === maxSlide - 1) {
+            curSlide = 0;
+        } else {
+            curSlide++;
+        }
+
+        goToSlide(curSlide);
+        
+    };
+
+    const prevSlide = function () {
+        if (curSlide === 0) {
+          curSlide = maxSlide - 1;
+        } else {
+          curSlide--;
+        }
+        goToSlide(curSlide);
+        
+    };
+
+    const init = function() {
+        goToSlide(0);
+        disableXScroll();
+    };
+    init();
+    
+
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', prevSlide);
+
+    
+
+};
+slider();
