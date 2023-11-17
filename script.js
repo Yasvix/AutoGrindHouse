cbca196d7a4f67fa67de6d516f597c155cc2006f
RezixDev <mateuszrysownik2@gmail.com>
2023-11-17 19:38:40 +0100

    fix: gallery

--- script.js
+++ script.js
@@ -1,79 +1,74 @@
 'strict mode';
 
+// Sidebar toggle
 const btnClose = document.querySelector('.sideBarClose');
 const btnOpen = document.querySelector('.sideBarOpen');
 const navbar = document.querySelector('.navbar');
 
-
-
-btnClose.addEventListener('click', function() {
-    navbar.style.display = "none";
+btnClose.addEventListener('click', function () {
+  navbar.style.display = 'none';
 });
 
-
-btnOpen.addEventListener('click', function() {
-    navbar.style.display = "flex";
+btnOpen.addEventListener('click', function () {
+  navbar.style.display = 'flex';
 });
-
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
 
 ///////////////////////////////////////
 // Slider
 
-const slider = function() {
-    const slides = document.querySelectorAll('.slide');
+const slider = function (gallerySelector) {
+  const slides = document.querySelectorAll(`${gallerySelector} .slide`);
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
+  // Function to disable horizontal scroll
+  const disableXScroll = function () {
+    document.body.style.overflowX = 'hidden';
+  };
 
-    let curSlide = 0;
-    const maxSlide = slides.length;
+  // Function to go to a specific slide
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${300 * (i - slide)}%)`)
+    );
+  };
 
-    const disableXScroll = function() {
-        document.body.style.overflowX = 'hidden';
-
+  // Function to go to the next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
     }
+    goToSlide(curSlide);
+  };
 
-    const goToSlide = function(slide) {
-        slides.forEach(
-            (s, i) => (s.style.transform = `translateX(${300 * (i - slide)}%)`)
-        );
-    };
+  // Function to go to the previous slide
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+  };
 
-    const nextSlide = function() {
-        if(curSlide === maxSlide - 1) {
-            curSlide = 0;
-        } else {
-            curSlide++;
-        }
+  // Initialize the slider
+  const init = function () {
+    goToSlide(0);
+    disableXScroll();
+  };
+  init();
 
-        goToSlide(curSlide);
-        
-    };
-
-    const prevSlide = function () {
-        if (curSlide === 0) {
-          curSlide = maxSlide - 1;
-        } else {
-          curSlide--;
-        }
-        goToSlide(curSlide);
-        
-    };
-
-    const init = function() {
-        goToSlide(0);
-        disableXScroll();
-    };
-    init();
-    
-
-    btnRight.addEventListener('click', nextSlide);
-    btnLeft.addEventListener('click', prevSlide);
-
-    
-
+  // Event listeners for next/previous buttons
+  document
+    .querySelector(`${gallerySelector} .slider__btn--right`)
+    .addEventListener('click', nextSlide);
+  document
+    .querySelector(`${gallerySelector} .slider__btn--left`)
+    .addEventListener('click', prevSlide);
 };
-slider();
 
-
-
+// Initialize sliders for each gallery
+slider('#auto-image-gallery');
+slider('#opinions-gallery');
